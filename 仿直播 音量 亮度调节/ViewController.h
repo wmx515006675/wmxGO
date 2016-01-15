bfcd11cf0820da6e579d2ca07048822046df0998
王洺轩 <wangmingxuan@jiubugaosuni.local>
2016-01-15 13:26:20 +0800

    Initial Commit

---
+++ 仿直播 音量 亮度调节/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿直播 音量 亮度调节
+//
+//  Created by 王洺轩 on 16/1/15.
+//  Copyright © 2016年 王洺轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 仿直播 音量 亮度调节/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  仿直播 音量 亮度调节
+//
+//  Created by 王洺轩 on 16/1/15.
+//  Copyright © 2016年 王洺轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
